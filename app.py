c5a6520a9b5f103582b5cc1124c23879e5984838
Dasun <techsaralk.pro@gmail.com>
2024-12-13 14:43:03 +0530

    CORS config added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app: FastAPI = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=['*'],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
 
 @app.get("/")
 async def index()->dict[str, str]:
