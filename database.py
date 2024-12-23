35ea23b791de2ee774df9f8003c4c0d826a6784d
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-23 21:44:17 +0530

    comments added to database models

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
+# imports
 from datetime import datetime
 from sqlmodel import Field, SQLModel, Session, create_engine, select
 
+# database models
 class User(SQLModel, table=True):
+    '''User model for the database. This model contains the user's information.'''
     id: int = Field(primary_key=True, default=None)
     username: str = Field(max_length=100)
     email: str = Field(max_length=100)
     password: str = Field(max_length=100)
 
 class Todo(SQLModel, table=True):
+    '''Todo model for the database. This model contains the todo information.'''
     id: int|None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     title: str = Field(max_length=100)
@@ -16,7 +20,7 @@ class Todo(SQLModel, table=True):
     created_at: datetime = Field(default_factory=lambda: datetime.now())
 
 class Studies(SQLModel, table=True):
-    # This table contains how many hours a user worked in the particular date.
+    '''Studies model for the database. This model contains the studies information.'''
     id: int|None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     date: datetime = Field(default_factory=lambda: datetime.now())
