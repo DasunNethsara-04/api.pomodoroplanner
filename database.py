1974bfd222d57661749f9cf616053d5693e5bc66
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-01-29 14:19:18 +0530

    code formatted

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db() -> Generator[sessionmaker, Any, None]:
     db = SessionLocal()
     try:
